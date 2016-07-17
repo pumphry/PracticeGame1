50c9212a6a688231d044268212cd72489172ac40
Alexander John Murphy <alexjmurphy90@gmail.com>
2016-07-17 01:07:12 -0400

    Animating texture for the road.

---
+++ PracticeGame1/Assets/Resources/Scripts/Gameplay/MovingRoadTexture.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingRoadTexture : MonoBehaviour {
+
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if (rend != null)
+        {
+            float offset = Time.time * scrollSpeed;
+            rend.material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
+        }
+    }
+}
